9bdb75071a7475f4e8787184dd5ccd126670d8e4
Molly Curtin <patternandshape@gmail.com>
2016-03-18 21:54:23 -0700

    add final styling

--- js/user.js
+++ js/user.js
@@ -1,11 +1,12 @@
-var apiKey = require('../.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(user) {
   $('#repoList').empty();
   //list first 100 repos in descending order with username entered by user
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey + '&sort=created&order=desc&per_page=9999').then(function(response) {
     for (var i = 0; i <= response.length; i++) {
-      $('#repoList').append("<li>" + response[i].name + "</li>");
+      var lastUpdate = moment(response[i].updated_at).format('l');
+      $('#repoList').append("<li>" + "<span class='repo'>" + response[i].name + "</span>" + "<i class='fa fa-cloud-upload'></i>" + "<span class='updated'>" + lastUpdate + "</span>" + "</li>");
     }
   }).fail(function(error) {
     $('#repoList').text("Username not found");
@@ -15,3 +16,5 @@ exports.getRepos = function(user) {
 
 // moment().format("MMM Do YY"); what to do with you?
 // var expires = moment.utc(date).format();
+
+moment().format('l');
